b2967aa4d02e7791c7a288a10ab3dabf7faaaea5
Jovan Bjegović <jovan.bjegovic@logeecom.com>
2025-10-20 16:18:07 +0200

    issue IONJ-206, add migration and DbContext changes

--- OnlineShop.Api/Program.cs
+++ OnlineShop.Api/Program.cs
@@ -4,8 +4,22 @@ using OnlineShop.Core;
 using OnlineShop.Core.Validators;
 using Microsoft.EntityFrameworkCore;
 using OnlineShop.Data;
+using DotNetEnv;
+
+Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
+
+var dbHost = Environment.GetEnvironmentVariable("POSTGRES_HOST") ?? "localhost";
+var dbPort = Environment.GetEnvironmentVariable("POSTGRES_PORT") ?? "5432";
+var dbName = Environment.GetEnvironmentVariable("POSTGRES_DB");
+var dbUser = Environment.GetEnvironmentVariable("POSTGRES_USER");
+var dbPass = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+
+var connectionString = $"Host={dbHost};Port={dbPort};Database={dbName};Username={dbUser};Password={dbPass}";
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString, b => b.MigrationsAssembly("OnlineShop.Data")));
 
 builder.Services
     .AddFluentValidationAutoValidation()
@@ -20,12 +34,6 @@ builder.Services
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
-        b => b.MigrationsAssembly("OnlineShop.Data")
-    ));
 
 var app = builder.Build();
 
--- OnlineShop.Data/AppDbContext.cs
+++ OnlineShop.Data/AppDbContext.cs
@@ -6,4 +6,30 @@ namespace OnlineShop.Data;
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+            
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(c => c.Id);
+
+            entity.Property(c => c.Title)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            entity.Property(c => c.Code)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            entity.Property(c => c.Description)
+                .HasMaxLength(500);
+
+            entity.HasMany(c => c.Subcategories)
+                .WithOne()
+                .HasForeignKey(sc => sc.ParentCategoryId)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
+    }
 }
