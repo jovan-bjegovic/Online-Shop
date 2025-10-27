4e40674ae22634cca6cd1aaf39b2ae8c053eba82
Jovan Bjegović <jovan.bjegovic@logeecom.com>
2025-10-27 11:31:25 +0100

    issue IONJ-210, construct connection string in main migration class from environment file

--- OnlineShop.Migrations/Program.cs
+++ OnlineShop.Migrations/Program.cs
@@ -1,5 +1,4 @@
 ﻿using FluentMigrator.Runner;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace OnlineShop.Migrations;
@@ -8,12 +7,20 @@ internal class Program
 {
     private static void Main()
     {
-        var configuration = new ConfigurationBuilder()
-            .AddJsonFile("appsettings.json", optional: false)
-            .AddEnvironmentVariables()
-            .Build();
-        
-        string? connectionString = configuration.GetConnectionString("DefaultConnection");
+        string? host = Environment.GetEnvironmentVariable("POSTGRES_HOST");
+        string? port = Environment.GetEnvironmentVariable("POSTGRES_PORT");
+        string? db = Environment.GetEnvironmentVariable("POSTGRES_DB");
+        string? user = Environment.GetEnvironmentVariable("POSTGRES_USER");
+        string? password = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+
+        if (string.IsNullOrEmpty(host) || string.IsNullOrEmpty(port) ||
+            string.IsNullOrEmpty(db) || string.IsNullOrEmpty(user) || string.IsNullOrEmpty(password))
+        {
+            Console.WriteLine("ERROR: Postgres environment variables not set.");
+            return;
+        }
+
+        string connectionString = $"Host={host};Port={port};Database={db};Username={user};Password={password}";
         
         var serviceProvider = new ServiceCollection()
             .AddFluentMigratorCore()
