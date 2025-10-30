f79c477dfd3ef5ea228312e414b3ec5628b37cad
Jovan Bjegović <jovan.bjegovic@logeecom.com>
2025-10-30 13:25:33 +0100

    issue IONJ-213, move database connection string to appsettings.json

--- OnlineShop.CategoryCleanup/Program.cs
+++ OnlineShop.CategoryCleanup/Program.cs
@@ -1,30 +1,18 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using OnlineShop.CategoryCleanup.Workers;
 using OnlineShop.Core.Interfaces;
 using OnlineShop.Core.UseCases.Categories.DeleteExpired;
 using OnlineShop.Data;
 using OnlineShop.Data.Repositories;
 
-if (args.Length < 5)
-{
-    Console.WriteLine("Usage: dotnet OnlineShop.CategoryCleanup.dll <host> <port> <db> <user> <password>");
-    
-    return;
-}
-
-string host = args[0];
-string port = args[1];
-string db = args[2];
-string user = args[3];
-string password = args[4];
-
-string connectionString = $"Host={host};Port={port};Database={db};Username={user};Password={password}";
-
 var hostBuilder = Host.CreateDefaultBuilder()
-    .ConfigureServices((_, services) =>
+    .ConfigureServices((context, services) =>
     {
+        var connectionString = context.Configuration.GetConnectionString("DefaultConnection");
+    
         services.AddDbContext<AppDbContext>(options =>
             options.UseNpgsql(connectionString));
 
